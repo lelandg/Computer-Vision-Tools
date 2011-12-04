c3df8b37994ed5e8f1d9b82e8e7c8a69b16ed134
Leland Green <aboogieman@gmail.com>
2011-12-04 16:09:25 -0600

    Added Stereo tab

--- ComputerVisionTools/MainWindow.xaml.cs
+++ ComputerVisionTools/MainWindow.xaml.cs
@@ -18,6 +18,8 @@ namespace ComputerVisionTools
         public MainWindow()
         {
             InitializeComponent();
+            Initialize();
+            InitializeStereo();
         }
 
         protected void Initialize()
@@ -142,13 +144,12 @@ namespace ComputerVisionTools
                     _sizeOnSensor = ConvertMeterToCentimeter(sizeOnSensor);
                 else if ((string) comboBoxSizeOnSensorUnits.SelectionBoxItem == "km")
                     _sizeOnSensor = ConvertKilometerToCentimeter(sizeOnSensor);
-                CheckCanCalculate();
             }
             else
             {
                 _sizeOnSensor = 0.0;
-                if (buttonCalculate != null) buttonCalculate.IsEnabled = false;
             }
+            CheckCanCalculate();
         }
 
         private void OnTextBoxActualSizeChanged(object sender, TextChangedEventArgs e)
@@ -169,13 +170,12 @@ namespace ComputerVisionTools
                     _actualSize = ConvertMeterToCentimeter(actualSize);
                 else if ((string) comboBoxActualSizeUnits.SelectionBoxItem == "km")
                     _actualSize = ConvertKilometerToCentimeter(actualSize);
-                CheckCanCalculate();
             }
             else
             {
                 _actualSize = 0.0;
-                if (buttonCalculate != null) buttonCalculate.IsEnabled = false;
             }
+            CheckCanCalculate();
         }
 
         private void OnTextBoxDistanceFromLensChanged(object sender, TextChangedEventArgs e)
@@ -196,13 +196,12 @@ namespace ComputerVisionTools
                     _distanceFromLens = ConvertMeterToCentimeter(distanceFromLens);
                 else if ((string) comboBoxDistanceFromLensUnits.SelectionBoxItem == "km")
                     _distanceFromLens = ConvertKilometerToCentimeter(distanceFromLens);
-                CheckCanCalculate();
             }
             else
             {
                 _distanceFromLens = 0.0;
-                if (buttonCalculate != null) buttonCalculate.IsEnabled = false;
             }
+            CheckCanCalculate();
         }
 
         private void OnTextBoxFocalLengthChanged(object sender, TextChangedEventArgs e)
@@ -223,13 +222,12 @@ namespace ComputerVisionTools
                     _focalLength = ConvertMeterToCentimeter(focalLength);
                 else if ((string) comboBoxFocalLengthUnits.SelectionBoxItem == "km")
                     _focalLength = ConvertKilometerToCentimeter(focalLength);
-                CheckCanCalculate();
             }
             else
             {
                 _focalLength = 0.0;
-                if (buttonCalculate != null) buttonCalculate.IsEnabled = false;
             }
+            CheckCanCalculate();
         }
 
         private void ComboBoxActualSizeUnitsDropDownClosed(object sender, EventArgs e)
@@ -250,6 +248,273 @@ namespace ComputerVisionTools
         private void ComboBoxFocalLengthUnitsDropDownClosed(object sender, EventArgs e)
         {
             UpdateFocalLength();
+        }
+        
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// Beginning of Stereo methods and fields
+        private double _sizeX1;
+        private double _sizeX2;
+        private double _xStereo; // Calculated from _sizeX1 - _sizeX2
+        private double _baseline;
+        private double _distanceFromLensStereo;
+
+        private Boolean _canCalculateStereo;
+        private double _focalLengthStereo;
+
+        protected void InitializeStereo()
+        {
+            _sizeX1 = 0.0;
+            _sizeX2 = 0.0;
+            _baseline = 0.0;
+            _distanceFromLensStereo = 0.0;
+            _focalLengthStereo = 0.0;
+            textBoxX1.Text = "";
+            textBoxX2.Text = "";
+            textBoxDistanceFromLensStereo.Text = "";
+            textBoxFocalLengthStereo.Text = "";
+            textBoxBaseline.Text = "";
+            _canCalculateStereo = false;
+            buttonCalculateStereo.IsEnabled = false;
+        }
+
+        private Boolean CanCalculateStereo()
+        {
+            textBlockFormulaStereo.Text = ""; // Only display formula right after Calculate is clicked.
+            _canCalculateStereo = false;
+            // We need 3 values to calculate
+            if (_sizeX1 == 0.0 && _sizeX2 == 0.0)
+            {
+                if (_baseline != 0.0 && _distanceFromLensStereo != 0.0 && _focalLengthStereo != 0.0) _canCalculateStereo = true;
+            }
+            else if (_baseline == 0.0)
+            {
+                if (_sizeX1 != 0.0 && _sizeX2 != 0.0 && _distanceFromLensStereo != 0.0 && _focalLengthStereo != 0.0) _canCalculateStereo = true;
+            }
+            else if (_distanceFromLensStereo == 0.0)
+            {
+                if (_baseline != 0.0 && _baseline != 0.0 && _focalLengthStereo != 0.0) _canCalculateStereo = true;
+            }
+            else if (_focalLengthStereo == 0.0)
+            {
+                if (_sizeX1 != 0.0 && _sizeX1 != 0.0 && _baseline != 0.0 && _distanceFromLensStereo != 0.0) _canCalculateStereo = true;
+            }
+            if (_canCalculateStereo)
+            {
+                buttonCalculateStereo.IsEnabled = true;
+            }
+            else
+            {
+                buttonCalculateStereo.IsEnabled = false;
+            }
+            if (_sizeX1 > _sizeX2) _xStereo = _sizeX1 - _sizeX2;
+            else _xStereo = _sizeX2 - _sizeX1;
+            labelDerivedX.Content = _xStereo.ToString();
+            return _canCalculateStereo;
+        }
+
+        private void CheckCanCalculateStereo()
+        {
+            CanCalculateStereo();
+        }
+        
+        private void TextBoxX1TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateX1();
+        }
+
+        private void comboBoxX1Units_DropDownClosed(object sender, EventArgs e)
+        {
+            UpdateX1();
+        }
+
+        private void UpdateX1()
+        {
+            Double x1;
+            if (Double.TryParse(textBoxX1.GetLineText(0), out x1))
+            {
+                if ((string)comboBoxX1Units.SelectionBoxItem == "mm")
+                    _sizeX1 = x1;
+                else if ((string)comboBoxX1Units.SelectionBoxItem == "cm")
+                    _sizeX1 = ConvertCentimeterToMillimeter(x1);
+                else if ((string)comboBoxX1Units.SelectionBoxItem == "m")
+                    _sizeX1 = ConvertMeterToCentimeter(x1);
+                else if ((string)comboBoxX1Units.SelectionBoxItem == "km")
+                    _sizeX1 = ConvertKilometerToCentimeter(x1);
+            }
+            else
+            {
+                _sizeX1 = 0.0;
+            }
+            CheckCanCalculateStereo();
+        }
+
+
+        private void TextBoxX2TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateX2();
+        }
+
+        private void ComboBoxX2UnitsDropDownClosed(object sender, EventArgs e)
+        {
+            UpdateX2();
+        }
+
+        private void UpdateX2()
+        {
+            Double x2;
+            if (Double.TryParse(textBoxX2.GetLineText(0), out x2))
+            {
+                if ((string)comboBoxX2Units.SelectionBoxItem == "mm")
+                    _sizeX2 = x2;
+                else if ((string)comboBoxX2Units.SelectionBoxItem == "cm")
+                    _sizeX2 = ConvertCentimeterToMillimeter(x2);
+                else if ((string)comboBoxX2Units.SelectionBoxItem == "m")
+                    _sizeX2 = ConvertMeterToCentimeter(x2);
+                else if ((string)comboBoxX2Units.SelectionBoxItem == "km")
+                    _sizeX2 = ConvertKilometerToCentimeter(x2);
+            }
+            else
+            {
+                _sizeX2 = 0.0;
+            }
+            CheckCanCalculateStereo();
+        }
+
+
+        private void OnTextBoxBaselineTextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateBaseline();
+        }
+
+        private void OnComboBoxBaselineUnitsDropDownClosed(object sender, EventArgs e)
+        {
+            UpdateBaseline();
+        }
+
+        private void UpdateBaseline()
+        {
+            Double baseline;
+            if (Double.TryParse(textBoxBaseline.GetLineText(0), out baseline))
+            {
+                if ((string)comboBoxBaselineUnits.SelectionBoxItem == "mm")
+                    _baseline = baseline;
+                else if ((string)comboBoxBaselineUnits.SelectionBoxItem == "cm")
+                    _baseline = ConvertCentimeterToMillimeter(baseline);
+                else if ((string)comboBoxBaselineUnits.SelectionBoxItem == "m")
+                    _baseline = ConvertMeterToCentimeter(baseline);
+                else if ((string)comboBoxBaselineUnits.SelectionBoxItem == "km")
+                    _baseline = ConvertKilometerToCentimeter(baseline);
+            }
+            else
+            {
+                _baseline = 0.0;
+            }
+            CheckCanCalculateStereo();
+        }
+
+
+        private void OnTextBoxDistanceFromLensStereoTextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateDistanceFromLensStereo();
+        }
+
+        private void OnComboBoxDistanceFromLensStereoUnitsDropDownClosed(object sender, EventArgs e)
+        {
+            UpdateDistanceFromLensStereo();
+        }
+
+        private void UpdateDistanceFromLensStereo()
+        {
+            Double distanceFromLensStereo;
+            if (Double.TryParse(textBoxDistanceFromLensStereo.GetLineText(0), out distanceFromLensStereo))
+            {
+                if ((string)comboBoxDistanceFromLensUnitsStereo.SelectionBoxItem == "mm")
+                    _distanceFromLensStereo = distanceFromLensStereo;
+                else if ((string)comboBoxDistanceFromLensUnitsStereo.SelectionBoxItem == "cm")
+                    _distanceFromLensStereo = ConvertCentimeterToMillimeter(distanceFromLensStereo);
+                else if ((string)comboBoxDistanceFromLensUnitsStereo.SelectionBoxItem == "m")
+                    _distanceFromLensStereo = ConvertMeterToCentimeter(distanceFromLensStereo);
+                else if ((string)comboBoxDistanceFromLensUnitsStereo.SelectionBoxItem == "km")
+                    _distanceFromLensStereo = ConvertKilometerToCentimeter(distanceFromLensStereo);
+            }
+            else
+            {
+                _distanceFromLensStereo = 0.0;
+            }
+            CheckCanCalculateStereo();
+        }
+
+
+        private void OnTextBoxFocalLengthStereoTextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateFocalLengthStereo();
+        }
+
+        private void OnComboBoxFocalLengthStereoUnitsDropDownClosed(object sender, EventArgs e)
+        {
+            UpdateFocalLengthStereo();
+        }
+
+        private void UpdateFocalLengthStereo()
+        {
+            Double focalLengthStereo;
+            if (Double.TryParse(textBoxFocalLengthStereo.GetLineText(0), out focalLengthStereo))
+            {
+                if ((string)comboBoxFocalLengthUnitsStereo.SelectionBoxItem == "mm")
+                    _focalLengthStereo = focalLengthStereo;
+                else if ((string)comboBoxFocalLengthUnitsStereo.SelectionBoxItem == "cm")
+                    _focalLengthStereo = ConvertCentimeterToMillimeter(focalLengthStereo);
+                else if ((string)comboBoxFocalLengthUnitsStereo.SelectionBoxItem == "m")
+                    _focalLengthStereo = ConvertMeterToCentimeter(focalLengthStereo);
+                else if ((string)comboBoxFocalLengthUnitsStereo.SelectionBoxItem == "km")
+                    _focalLengthStereo = ConvertKilometerToCentimeter(focalLengthStereo);
+            }
+            else
+            {
+                _focalLengthStereo = 0.0;
+            }
+            CheckCanCalculateStereo();
+        }
+
+
+        private void ButtonCalculateStereoClick(object sender, RoutedEventArgs e)
+        {
+            if (CanCalculateStereo()) // Shouldn't be here if we can't calculate, but check to be sure.
+            {
+                if (_xStereo == 0.0)
+                {
+                    _xStereo = _baseline*(_focalLengthStereo/_distanceFromLensStereo);
+                    labelDerivedX.Content = _xStereo.ToString();
+                    //comboBoxSizeOnSensorUnits.SelectedIndex = 0;
+                    textBlockFormulaStereo.Text = "X = B * (f / Z)";
+                }
+                else if (_baseline == 0.0)
+                {
+                    _baseline = _xStereo/(_focalLengthStereo/_distanceFromLensStereo);
+                    textBoxBaseline.Text = _baseline.ToString();
+                    comboBoxBaselineUnits.SelectedIndex = 0;
+                    textBlockFormulaStereo.Text = "B = x1 - x2 / (f / Z)";
+                }
+                else if (_distanceFromLensStereo == 0.0)
+                {
+                    _distanceFromLensStereo = (_focalLengthStereo*_baseline)/_xStereo;
+                    textBoxDistanceFromLensStereo.Text = _distanceFromLensStereo.ToString();
+                    comboBoxDistanceFromLensUnitsStereo.SelectedIndex = 0;
+                    textBlockFormulaStereo.Text = "Z = (f * B) / x1 - x2)";
+                }
+                else if (_focalLengthStereo == 0.0)
+                {
+                    _focalLengthStereo = _xStereo*(_distanceFromLensStereo/_baseline);
+                    textBoxFocalLengthStereo.Text = _focalLengthStereo.ToString();
+                    comboBoxFocalLengthUnits.SelectedIndex = 0;
+                    textBlockFormulaStereo.Text = "f = (x1 - x2) * (Z / B)";
+                }
+            }
+        }
+
+        private void ButtonResetStereoClick(object sender, RoutedEventArgs e)
+        {
+            InitializeStereo();
         }
 
     }
